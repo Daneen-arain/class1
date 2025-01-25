f7683e73ccbf7a33d76f3f68a97e6c6a21867b72
Daneen-arain <araindaneen43@gmail.com>
2025-01-26 00:51:03 +1000

    Add files via upload

---
+++ variousvalues.py
@@ -0,0 +1,9 @@
+
+a = 5
+b = 'flabbergasted'
+
+print(a, b)
+
+print('The two variable values are:\n', a, '\n', 'and', b)
+
+print('A paragraph is a section of \nwriting that presents a single \nidea or point about a topic. \n Paragraphs are the building blocks of essays and other written\n works.')
