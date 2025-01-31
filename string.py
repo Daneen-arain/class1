0f50ae2c4508bc1d2800ca69a5aeb6213ac7832e
Daneen-arain <araindaneen43@gmail.com>
2025-02-01 00:52:08 +1000

    Add files via upload

---
+++ string.py
@@ -0,0 +1,12 @@
+str1 ='Hello \n'
+str2 ='There!'
+
+print('concatinated value is :', str1 + str2)
+
+print('now slicicng characters! ')
+
+str3 = 'tubthumping'
+
+print(str3[::1])
+str 4='the the the the the
+
