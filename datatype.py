8b5e371cb9cfb3c21eac76b1717293f81c4b5b89
Daneen-arain <araindaneen43@gmail.com>
2025-02-01 00:20:59 +1000

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,40 @@
+a = 5
+print(5)  
+print("Type of 5:", type(5))  
+
+b=2.5
+print(2.5)  
+print("Type of 2.5:", type(2.5))  
+
+c= "Hello there"
+print("Hello")  
+print("Type of 'Hello':", type("Hello"))  
+
+d= True
+print(True)  
+print("Type of True:", type(True)) 
+
+
+is_student = True
+weight = 60
+age = False
+
+
+print("is_student :", is_student)
+print("Data Type of is_student is", type(is_student))
+
+print("weight :", weight)
+print("Data Type of weight is", type(weight))
+
+
+print("\nAfter Type Casting...")
+
+age = bool(age)
+print(age)
+print("Data Type of age is", type(age))
+
+weight = int(weight)
+print(weight)
+print("Data Type of weight is", type(weight))
+
+
