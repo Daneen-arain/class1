b6ce5f8a57680483731d88945a98439f09b2d5bf
Daneen-arain <araindaneen43@gmail.com>
2025-02-02 00:58:19 +1000

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,22 @@
+m1 = 96
+m2 = 94
+m3 = 5
+m4 = 59
+m1 = 93
+m2 = 93
+m3 = 5
+m4 = 89
+
+average = (m1 + m2 + m3 + m4) 
+
+if m2 != m1:
+    print('Hi')
+else:
+    print('Hello')
+
+print("Average:", average)  
+
+if m2 != m1:
+    print('Hi')
+else:
+    print('Hello')
---
+++ notes.py
@@ -0,0 +1,15 @@
+
+Amount = 780
+
+cal1 = 780 / 43
+cal2 = 780 // 43
+
+print('Normal division:', cal1, 'Floor division:', cal2)
+
+
+note_100 = Amount 
+note_50 = (Amount % 100) 
+note_10 = ((Amount % 100) % 50) 
+print("Notes of 100 rupee:", note_100)
+print("Notes of 50 rupee:", note_50)
+print("Notes of 10 rupee:", note_10)
