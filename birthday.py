c2b60dd360cb773957c75c958f27260391b185a2
Daneen-arain <araindaneen43@gmail.com>
2025-02-01 21:10:47 +1000

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,14 @@
+# Storing five different birthday dates
+birthday1 = "January 15, 2000"
+birthday2 = "March 22, 1998"
+birthday3 = "July 8, 2005"
+birthday4 = "October 31, 1995"
+birthday5 = "December 25, 2010"
+
+# Printing the birthdays
+print("Stored Birthdays:")
+print("1.", birthday1)
+print("2.", birthday2)
+print("3.", birthday3)
+print("4.", birthday4)
+print("5.", birthday5)
