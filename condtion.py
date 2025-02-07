be122a2744c33c057cc47d6171fc7205ba3d8dfb
Daneen-arain <araindaneen43@gmail.com>
2025-02-07 23:53:22 +1000

    Add files via upload

---
+++ condtion.py
@@ -0,0 +1,11 @@
+a = int(input('enter a number : '))
+
+if a > 0:
+    print('It is a positive number!')
+
+elif a == 0:
+    print('Its just 0!')
+    b = float(input('enter a decimal number : '))
+
+else:
+    print('Its a negative number')
