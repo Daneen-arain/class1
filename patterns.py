76cec8b85783d4ac0172239569ae60f92f2b03e8
Daneen-arain <araindaneen43@gmail.com>
2025-03-02 00:10:15 +1000

    Add files via upload

---
+++ patterns.py
@@ -0,0 +1,9 @@
+print("Reverse Half Pyramid Pattern:")
+n = int(input("Enter the number of rows: "))
+
+
+for i in range(n, 0, -1):
+ 
+    for j in range(i):
+        print("@", end="")
+    print()  
