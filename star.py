6c59fa58a54e6e69898c19dde9cd131b15ad623a
Daneen-arain <araindaneen43@gmail.com>
2025-03-08 00:29:54 +1000

    Add files via upload

---
+++ star.py
@@ -0,0 +1,34 @@
+import turtle
+turtle.Screen().bgcolor("salmon")
+t = turtle.Turtle()
+
+# First triangle
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.penup()
+
+t.right(150)
+
+t.forward(50)
+
+
+# second triangle for star
+
+t.pendown()
+
+t.right(90)
+
+t.forward(100)
+
+t.right(120)
+
+t.forward(100)
+
+t.right(120)
+
+t.forward(100)
+
+turtle.done()
