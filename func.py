d171b9225c85751472f35879de1d3a24d1a1b23a
Daneen-arain <araindaneen43@gmail.com>
2025-03-09 00:36:30 +1000

    Add files via upload

---
+++ func.py
@@ -0,0 +1,17 @@
+def random():
+    print('Today is Saturday night! Time to party!')
+    print('Today is codingal class at 12am Australia time! Time to code! ')
+    print('Today were doing a class about Function?! Here i come!')
+
+def day():
+    print ('Today is Saturday night! Time to sleep')
+    print('Today is Sunday! I hate sundays!')
+    print('Today is monday ! i hate school days!')
+def print1():
+
+    day()
+    random()
+    
+print1()
+
+          
