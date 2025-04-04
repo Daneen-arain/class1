58b2a8a84e6829156828fe0789f1b223cd9becb8
Daneen-arain <araindaneen43@gmail.com>
2025-04-05 00:10:15 +1000

    Add files via upload

---
+++ lists1.py
@@ -0,0 +1,22 @@
+a = []
+print(a)
+
+a = [1, 2, 3, 4, 5]
+print(a)
+
+r = a[::-1]
+print(r)
+
+triples = [1, 2, 3] * 4
+print(triples)
+
+s = "cat" 
+reversed_s = s[::-1]
+print(reversed_s)
+
+alist = [100, 200, 300, 400, 500]
+alist = alist[::-1]
+print(alist, "\n")
+
+for i in alist:
+    print(i)
